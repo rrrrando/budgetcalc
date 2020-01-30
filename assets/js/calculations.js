4de332c14a0119b774b23bbc744ef74c63227b54
Janek Mander <mander.j@gmail.com>
2020-01-30 14:27:10 +0200

    Added localstorage logic

--- assets/js/calculations.js
+++ assets/js/calculations.js
@@ -1,28 +1,83 @@
+var storedAddRows = [];
+var storedMinusRows = [];
+var storedTotalSum = 0;
+
+displayLocalStorage();
 
 function addBudgetRow() {
 
-    let sum_total_container = document.querySelector('#sum_total');
-    var total_sum = parseFloat(sum_total_container.dataset.sum);
+    getLocalStorageData();
 
     let description = document.getElementById("description").value;
     let sum = parseFloat(document.getElementById("sum").value);
 
     let action = document.querySelector('input[name="action"]:checked').value;
 
-    var node = document.createElement("li");
-    var textnode = document.createTextNode("description | " + sum);
-    node.appendChild(textnode);
+    let object = {
+        description: description,
+        sum: sum
+    };
 
     if (action == 'add') {
-        total_sum += sum;
-        document.getElementById("add").appendChild(node);
+        storedTotalSum += sum;
+        save(storedAddRows, object, "add_rows");
     } else {
-        total_sum -= sum;
-        document.getElementById("minus").appendChild(node);
-
+        storedTotalSum -= sum;
+        save(storedMinusRows, object, "minus_rows");
     }
 
-    document.getElementById("sum_total").innerHTML = total_sum;
-    sum_total_container.dataset.sum = total_sum;
+    document.getElementById("sum_total").innerHTML = storedTotalSum;
 
+    localStorage.setItem("total_sum", storedTotalSum);
+
+}
+
+function displayLocalStorage() {
+
+    getLocalStorageData();
+
+    for (let row in storedAddRows) {
+        let object = storedAddRows[row];
+        show(object, '#add');
+    }
+
+    for (let row in storedMinusRows) {
+        let object = storedMinusRows[row];
+        show(object, '#minus');
+    }
+
+    document.getElementById("sum_total").innerHTML = storedTotalSum;
+
+}
+
+function save(objects, object, key) {
+
+    objects.push(object);
+    localStorage.setItem(key, JSON.stringify(objects));
+
+    key = key == 'add_rows' ? '#add' : '#minus';
+    show(object, key);
+}
+
+function show(object, key) {
+    let node = document.createElement("li");
+    let row = document.createTextNode(object.description + " | " + object.sum);
+    node.appendChild(row);
+    document.querySelector(key).appendChild(node);
+}
+
+function getLocalStorageData() {
+
+    storedAddRows = JSON.parse(localStorage.getItem("add_rows"));
+    storedAddRows = storedAddRows == null ? [] : storedAddRows;
+
+    storedMinusRows = JSON.parse(localStorage.getItem("minus_rows"));
+    storedMinusRows = storedMinusRows == null ? [] : storedMinusRows;
+
+    storedTotalSum = localStorage.getItem("total_sum");
+    storedTotalSum = storedTotalSum == null ? 0 : parseFloat(localStorage.getItem("total_sum"));
+}
+
+function clearLocalStorage() {
+    localStorage.clear();
 }
