07d19bad3bec887e09d0d20c40a919a9f6c3424a
Rando Õispuu <rando@innofly.eu>
2020-01-31 13:58:22 +0200

    Fixes and animation

--- assets/js/calculations.js
+++ assets/js/calculations.js
@@ -1,83 +1,56 @@
 let storedAddRows = [];
 let storedMinusRows = [];
 let storedTotalSum = 0;
+let description = document.getElementById("comment");
+let sum = document.getElementById("sum");
 const addRowBtn = document.getElementById('add-row-btn');
+const type = document.querySelector('#switch-checkbox');
+const totalSum = document.querySelector(".total-sum");
+const showBtn = document.querySelector(".add-btn");
+const wrapper = document.querySelector(".wrapper");
+const showData = document.querySelector(".show-data");
+const cancel = document.querySelector(".cancel");
 
 addRowBtn.addEventListener('click', addBudgetRow);
+showBtn.addEventListener('click', toggleDialog);
+cancel.addEventListener('click', toggleDialog);
 
-// displayLocalStorage();
+function toggleDialog() {
+    wrapper.classList.toggle('open');
+    showData.classList.toggle('open');
+}
 
 function addBudgetRow() {
 
-//    console.log(123);
+    if (description.value === '' || sum.value === '') return;
 
-//    let alert = document.querySelector("#alert");
-//    alert.style.display = 'none';
-
-    getLocalStorageData();
-
-    let description = document.getElementById("comment").value;
-    let sum = +document.getElementById("sum").value;
-
-    console.log(description);
-    console.log(sum);
-
-    // if (description == "") {
-    //     alert.style.display = 'block';
-    //     alert.innerHTML = 'Description is empty';
-    //     return;
-    // }
-
-    // if (isNaN(sum) || typeof sum != 'number' || sum == 0) {
-    //     alert.style.display = 'block';
-    //     alert.innerHTML = 'Sum is empty or 0';
-    //     return;
-    // }
-
-    let action = document.querySelector('#switch-checkbox').checked ? 'expense' : 'income';
-    console.log(action);
+    let action = type.checked ? 'expense' : 'income';
 
     let object = {
-        description: description,
-        sum: sum
+        description: description.value,
+        sum: parseFloat(sum.value)
     };
 
     if (action == 'income') {
-        storedTotalSum += sum;
+        storedTotalSum += object.sum;
         save(storedAddRows, object, "income");
     } else {
-        storedTotalSum -= sum;
+        storedTotalSum -= object.sum;
         save(storedMinusRows, object, "expense");
     }
 
-    document.getElementById("sum_total").innerHTML = storedTotalSum;
+    totalSum.innerHTML = storedTotalSum.toFixed(2);
 
     localStorage.setItem("total_sum", storedTotalSum);
 
-}
-
-function displayLocalStorage() {
-
-    getLocalStorageData();
-
-    for (let row in storedAddRows) {
-        let object = storedAddRows[row];
-        show(object, 'expense');
-    }
-
-    for (let row in storedMinusRows) {
-        let object = storedMinusRows[row];
-        show(object, 'income');
-    }
-
-    document.querySelector(".total-sum").innerHTML = storedTotalSum;
+    description.value = ''
+    sum.value = ''
 
 }
 
 function save(objects, object, key) {
 
     objects.push(object);
-    localStorage.setItem(key, JSON.stringify(objects));
 
     key = key == 'income' ? 'income' : 'expense';
     show(object, key);
@@ -85,32 +58,19 @@ function save(objects, object, key) {
 
 function show(object, key) {
     let item = `
-<li class="data-item ${key} flex">
-    <div class="flex py items-center">
-        <div class="amount">${object.sum}</div>
-        <div class="desc">${object.description}</div>
-    </div>
-    <div class="label"></div>
-</li>`;
+        <div class="flex py ${key === 'income' ? 'order-1' : ''} items-center">
+            <div class="amount">${object.sum.toFixed(2)}</div>
+            <div class="desc">${object.description}</div>
+        </div>
+        <div class="label"></div>
+   `;
 
+    const li = document.createElement('li');
+    li.innerHTML = item;
+    li.classList.add('data-item', key, 'flex');
 
-    document.querySelector(".data").innerHTML += item;
-}
-
-function getLocalStorageData() {
-
-    storedAddRows = JSON.parse(localStorage.getItem("add_rows"));
-    storedAddRows = storedAddRows == null ? [] : storedAddRows;
-
-    storedMinusRows = JSON.parse(localStorage.getItem("minus_rows"));
-    storedMinusRows = storedMinusRows == null ? [] : storedMinusRows;
-
-    storedTotalSum = localStorage.getItem("total_sum");
-    storedTotalSum = storedTotalSum == null ? 0 : parseFloat(localStorage.getItem("total_sum"));
-}
-
-function clearLocalStorage() {
-    localStorage.clear();
-    document.querySelector("income").innerHTML = "";
-    document.querySelector("expense").innerHTML = "";
+    document.querySelector(".data").appendChild(li);
+    setTimeout(function () {
+        li.classList.add('show')
+    }, 10);
 }
